c56b3c1f7963e73440575a4ba1c3ca747d3dc6d7
ToxicBuZ <toxicbuz@gmail.com>
2025-09-18 11:04:52 +0300

    create first components, implement not-found component

--- weather-frontend/src/app/components/login/login.component.ts
+++ weather-frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-login',
-    imports: [],
+    standalone: true,
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
