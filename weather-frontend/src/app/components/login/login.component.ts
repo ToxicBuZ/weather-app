5ae0799ffbbdc3628dfaadb35b1fb445d4c6a668
ToxicBuZ <toxicbuz@gmail.com>
2025-09-18 12:35:14 +0300

    add prettierrc, eslintrc, primeng, primeflex, primeicons, basic routes and components, ui for login

--- weather-frontend/src/app/components/login/login.component.ts
+++ weather-frontend/src/app/components/login/login.component.ts
@@ -1,16 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { FormService } from '../../services/form.service';
+import { PasswordModule } from 'primeng/password';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss'
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ToastModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public form: FormGroup;
 
+  constructor(private formService: FormService,
+    private router: Router
+  ) {
+    this.form = this.formService.buildLoginForm();
+  }
+
+  ngOnInit(): void {}
+
+  public onSignIn() {
+    console.log('submitted');
+  }
+
+  public onSignUp() {
+    this.router.navigate(['/signup']);
+  }
 }
